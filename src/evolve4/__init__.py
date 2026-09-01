f83a593fbad557abd2444cc2d8daa705a9fc34a0
Roland <56701666+ReloadLightly@users.noreply.github.com>
2026-09-01 10:19:50 +0200

    Add EVOLVE IV package

---
+++ src/evolve4/__init__.py
@@ -0,0 +1,5 @@
+"""EVOLVE IV reconstruction: two metabolites, heritable conversion, niches."""
+
+from .simulation import MetabolicSim, MetabolicConfig
+
+__all__ = ["MetabolicSim", "MetabolicConfig"]
